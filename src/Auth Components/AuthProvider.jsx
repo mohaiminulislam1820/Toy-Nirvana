214f41b43c33878de6c44a9900100ec69b47f2ab
aaachris1820 <63766879+aaachris1820@users.noreply.github.com>
2023-05-18 07:42:25 +0600

    added auth context provider component with functions for signup and signin

--- src/Auth Components/AuthProvider.jsx
+++ src/Auth Components/AuthProvider.jsx
@@ -39,7 +39,7 @@ const AuthProvider = ({children}) => {
     }
 
     const contexts={
-        user,loading,setLoading,signInWithEmail,signUpWithEmail,signInWithGoogle,signInWithGithub
+        user,loading,setLoading,signInWithEmail,signUpWithEmail,signInWithGoogle
     };
 
 
