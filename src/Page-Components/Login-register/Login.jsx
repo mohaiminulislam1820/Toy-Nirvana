2f4dd5e9e95c8a39eba2bd0eb6f9f6f433721e8a
aaachris1820 <63766879+aaachris1820@users.noreply.github.com>
2023-05-18 10:27:05 +0600

    designed the sign up page

--- src/Page-Components/Login-register/Login.jsx
+++ src/Page-Components/Login-register/Login.jsx
@@ -4,12 +4,17 @@ import { useNavigate } from 'react-router-dom';
 const Login = () => {
     const navigate=useNavigate();
 
+    const handleSubmit=()=>{
+        
+    }
+
+
     return (
         <div className='mt-16 w-10/12 sm:w-8/12 lg:w-6/12 mx-auto border rounded-lg py-10 shadow-lg bg-slate-100'>
             <h1 className='text-center font-bold text-4xl mb-2'>Welcome Back!</h1>
             <p className='text-gray-600 text-center'>Login to coninue</p>
 
-            <form className='w-10/12 mx-auto mt-8'>
+            <form className='w-10/12 mx-auto mt-8' onSubmit={handleSubmit}>
 
             <div className="mb-6">
                     <label className="block  font-bold mb-4" htmlFor="email">
@@ -28,7 +33,7 @@ const Login = () => {
                 <button type='submit' className='px-10 py-3 font-bold text-white bg-[#219EBC] rounded-lg'>Sign In</button>
             </form>
 
-            <p className='text-center mt-4 text-gray-500'>Don't have an Account? <span onClick={()=>navigate('/register')} className='text-[#219EBC] font-bold'>Sign Up</span> here</p>
+            <p className='text-center mt-4 text-gray-500'>No Account? <span onClick={()=>navigate('/register')} className='text-[#219EBC] font-bold cursor-pointer'>Sign Up</span> here</p>
 
             <p className='text-center'>Or</p>
 
--- src/Page-Components/Login-register/Register.jsx
+++ src/Page-Components/Login-register/Register.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
+
+    const navigate = useNavigate();
+
+    const handleSubmit=()=>{
+
+    }
+
     return (
-        <div>
-            
+        <div className='mt-16 w-10/12 sm:w-8/12 lg:w-6/12 mx-auto border rounded-lg py-10 shadow-lg bg-slate-100'>
+
+            <h1 className='text-center font-bold text-4xl mb-4'>Sign Up</h1>
+
+            <p className='text-gray-400 text-center'>Already a member? <span onClick={() => navigate('/login')} className='text-[#219EBC] font-bold cursor-pointer' >Log In</span></p>
+
+            <form className='w-10/12 mx-auto mt-8' onSubmit={handleSubmit}>
+
+                <div className="mb-6">
+                    <label className="block  font-bold mb-4" htmlFor="email">
+                        Your Email
+                    </label>
+                    <input className="shadow  border rounded w-full py-3 px-3 " name="email" type="email" placeholder="Email" />
+                </div>
+
+                <div className="mb-6">
+                    <label className="block  font-bold mb-4" htmlFor="email">
+                        Your Password
+                    </label>
+                    <input className="shadow  border rounded w-full py-3 px-3 " name="password" type="password" placeholder="password ******" />
+                </div>
+
+                <div className="mb-6">
+                    <label className="block  font-bold mb-4" htmlFor="name">
+                        Your Name
+                    </label>
+                    <input className="shadow  border rounded w-full py-3 px-3 " name="name" type="text" placeholder="Name" />
+                </div>
+
+                <div className="mb-6">
+                    <label className="block  font-bold mb-4" htmlFor="photoURL">
+                        Photo URL
+                    </label>
+                    <input className="shadow  border rounded w-full py-3 px-3 " name="photoURL" type="url" placeholder="https://example.com/image.jpg" />
+                </div>
+
+                <button type='submit' className='px-10 py-3 font-bold text-white bg-[#219EBC] rounded-lg'>Sign Up</button>
+            </form>
+
         </div>
     );
 };
